21a02f673e92cb513c33c68f72c076649eb5af15
Uice Lu <uicestone@gmail.com>
2018-06-07 23:12:15 +0800

    下载党史的WP命令行

--- class.jddj-cli.php
+++ class.jddj-cli.php
@@ -16,8 +16,28 @@ class JDDJ_CLI extends WP_CLI_Command {
 	 *
 	 */
 	public function download_review() {
+		$cpc_review_urls = null;
+		require JDDJ__PLUGIN_DIR . 'resource/cpc-review.php';
+		foreach ($cpc_review_urls as $month => $urls) {
+			foreach ($urls as $day => $url) {
+				WP_CLI::line( 'Attempting: ' . $month . ' ' . $day );
+				$page_raw = file_get_contents($url);
+				$page = iconv('gb2312', 'utf-8//IGNORE', $page_raw);
+				preg_match('/\<h1 class\="red"\>党史上的今天\<\/h1\>\s*([\s\S]*?)<\/div>/', $page, $matches);
+				if (!$matches) {
+					WP_CLI::line( var_export($page_raw, true) ); exit;
+				}
+				$post_id = wp_insert_post(array(
+					'post_status' => 'publish',
+					'post_title' => '党史上的那一天：' . $month . '月' . $day . '日',
+					'post_name' => 'cpc-review-' . str_pad($month, 2, '0', STR_PAD_LEFT) . '-' . str_pad($day, 2, '0', STR_PAD_LEFT),
+					'post_content' => $matches[1],
+					'post_category' => ['cpc-review']
+				));
+				WP_CLI::line( 'Post inserted: ' . $post_id );
 
-		WP_CLI::line( 'Test result.' );
+			}
+		}
 
 	}
 }
