1b14826bc8df780c9abbf2838371d9b5c51e0d67
Uice Lu <uicestone@gmail.com>
2018-06-09 21:16:58 +0800

    座右铭管理后台

--- class.jddj-admin.php
+++ class.jddj-admin.php
@@ -19,8 +19,22 @@ class JDDJ_Admin {
 				'not_found' => '未找到党建声音'
 			),
 			'public' => true,
-			'supports' => array('title', 'editor', 'revisions', 'thumbnail', 'page-attributes'),
+			'supports' => array('title'),
 			'menu_icon' => 'dashicons-megaphone',
+			'has_archive' => true
+		));
+
+		register_post_type('motto', array(
+			'label' => '座右铭',
+			'labels' => array(
+				'all_items' => '所有座右铭',
+				'add_new' => '添加座右铭',
+				'add_new_item' => '新座右铭',
+				'not_found' => '未找到座右铭'
+			),
+			'public' => true,
+			'supports' => array('title', 'editor'),
+			'menu_icon' => 'dashicons-clipboard',
 			'has_archive' => true
 		));
 	}
@@ -43,6 +57,31 @@ class JDDJ_Admin {
 				case 'audio' :
 					$url = get_post_meta($post->ID, 'audio_url', true);
 					echo '<audio src="' . $url . '" controls></audio>';
+					break;
+				default;
+			}
+		});
+
+		add_filter('manage_motto_posts_columns', function($columns) {
+			$columns['text'] = '文字';
+			$columns['author_name'] = '作者';
+			$columns['image'] = '图片';
+			return $columns;
+		});
+
+		add_action('manage_motto_posts_custom_column', function($column_name) {
+			global $post;
+			switch ($column_name ) {
+				case 'text' :
+					echo $post->post_content;
+					break;
+				case 'author_name' :
+					$author_name = get_post_meta($post->ID, 'author_name', true);
+					echo $author_name;
+					break;
+				case 'image' :
+					$image_url = get_post_meta($post->ID, 'image_url', true);
+					echo '<a href="' . $image_url . '" target="_blank">' . basename($image_url) . '</a>';
 					break;
 				default;
 			}
