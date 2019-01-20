e8021af2560db91aa3f3bbb8b37c1853b8c637e2
Uice Lu <uicestone@gmail.com>
2019-01-21 03:28:06 +0800

    短信验证码和个人中心相关接口

--- rest-controllers/class.pba-rest-misc-controller.php
+++ rest-controllers/class.pba-rest-misc-controller.php
@@ -31,6 +31,34 @@ class PBA_REST_Misc_Controller extends WP_REST_Controller {
 				'callback' => array( $this, 'sign_in' ),
 			)
 		) );
+
+		register_rest_route( $this->namespace, '/verify-mobile', array(
+			array(
+				'methods' => WP_REST_Server::READABLE,
+				'callback' => array( $this, 'verify_mobile' ),
+			)
+		) );
+
+		register_rest_route( $this->namespace, '/my-sign-in', array(
+			array(
+				'methods' => WP_REST_Server::READABLE,
+				'callback' => array( $this, 'get_my_sign_in' ),
+			)
+		) );
+
+		register_rest_route( $this->namespace, '/my-yuyue', array(
+			array(
+				'methods' => WP_REST_Server::READABLE,
+				'callback' => array( $this, 'get_my_yuyue' ),
+			)
+		) );
+
+		register_rest_route( $this->namespace, '/my-speech', array(
+			array(
+				'methods' => WP_REST_Server::READABLE,
+				'callback' => array( $this, 'get_my_speech' ),
+			)
+		) );
 	}
 
 	/**
@@ -70,7 +98,7 @@ class PBA_REST_Misc_Controller extends WP_REST_Controller {
 	}
 
 	/**
-	 * Post sign in data
+	 * Post sign-in data
 	 *
 	 * @param WP_REST_Request $request
 	 * @return WP_Error|WP_REST_Response
@@ -119,5 +147,142 @@ class PBA_REST_Misc_Controller extends WP_REST_Controller {
 		}
 
 		return rest_ensure_response($member_info);
+	}
+
+	/**
+	 * Verify mobile
+	 *
+	 * @param WP_REST_Request $request
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public static function verify_mobile( $request ) {
+		$mobile = preg_replace('/[\s\-]/', '', $request->get_param('mobile'));
+		$code = $request->get_param('code');
+
+		if ($code) {
+			$code_option = json_decode(get_option('mobile_code_' . $mobile));
+			if (isset($code_option->code) && $code_option->code === $code && $code_option->expires_at >= time()) {
+				$token = $mobile . ' ' . sha1($mobile . '-' . NONCE_KEY);
+				return rest_ensure_response(array('token' => $token));
+			}
+			return rest_ensure_response(new WP_Error(403, '短信验证码错误'));
+		} else {
+			$code_option = json_decode(get_option('mobile_code_' . $mobile));
+			if (!isset($code_option->code) || $code_option->expires_at < time()) {
+				$code = rand(1000, 9999);
+				update_option('mobile_code_' . $mobile, json_encode(array('code'=>(string)$code, 'expires_at'=>time()+600)));
+			} else {
+				$code = $code_option->code;
+			}
+			error_log('[DEBUG] SMS code is ' . $code);
+			if (function_exists('aliyun_send_mobile_code')) {
+				error_log('Fail to send SMS, function aliyun_send_mobile_code not exists.');
+				$result = aliyun_send_mobile_code($mobile, $code);
+				return rest_ensure_response(array('message' => '短信验证码已发送'));
+			}
+			return rest_ensure_response(new WP_Error(500, '短信服务不可用'));
+		}
+
+	}
+
+	/**
+	 * My sign-in data
+	 *
+	 * @param WP_REST_Request $request
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public static function get_my_sign_in( $request )
+	{
+		$token = $_SERVER['HTTP_AUTHORIZATION'];
+		$token_parts = explode(' ', $token);
+		$mobile = $token_parts[0];
+		$hash = $token_parts[1];
+		if ($hash === sha1($mobile . '-' . NONCE_KEY)) {
+
+			$users_matched = get_users(array('meta_query' => array(
+				array('key' => 'mobile', 'value' => $mobile)
+			)));
+
+			if (count($users_matched) === 1) {
+				$user_id = $users_matched[0]->ID;
+			}
+			elseif(empty($user_id)) {
+				return rest_ensure_response(new WP_Error(400, '匹配用户错误'));
+			}
+
+			$user_fields = array('name', 'unit', 'organization');
+
+			$user_data = array();
+
+			foreach ($user_fields as $user_field) {
+				$user_data[$user_field] = get_user_meta($user_id, $user_field, true);
+			}
+
+			return rest_ensure_response($user_data);
+		}
+	}
+
+	/**
+	 * My yuyue data
+	 *
+	 * @param WP_REST_Request $request
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public static function get_my_yuyue( $request )
+	{
+		$token = $_SERVER['HTTP_AUTHORIZATION'];
+		$token_parts = explode(' ', $token);
+		$mobile = $token_parts[0];
+		$hash = $token_parts[1];
+		if ($hash === sha1($mobile . '-' . NONCE_KEY)) {
+
+			$posts = get_posts(array('post_type' => 'appointment', 'posts_per_page' => -1, 'meta_query' => array(
+				array('key' => '联系电话', 'value' => $mobile)
+			)));
+
+			$appointments = array_map(function($post) {
+				return array(
+					'target' => get_post_meta($post->ID, 'target', true)
+				);
+			}, $posts);
+
+			return rest_ensure_response($appointments);
+
+		}
+	}
+
+	/**
+	 * My speech data
+	 *
+	 * @param WP_REST_Request $request
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public static function get_my_speech( $request )
+	{
+		$token = $_SERVER['HTTP_AUTHORIZATION'];
+		$token_parts = explode(' ', $token);
+		$mobile = $token_parts[0];
+		$hash = $token_parts[1];
+		if ($hash === sha1($mobile . '-' . NONCE_KEY)) {
+
+			$posts = get_posts(array('post_type' => 'speech', 'posts_per_page' => -1, 'meta_query' => array(
+				array('key' => 'author_mobile', 'value' => $mobile)
+			)));
+
+			$speeches = array_map(function (WP_Post $post) {
+				$speech = array(
+					'id' => $post->ID,
+					'type' => get_post_meta($post->ID, 'type', true),
+					'bgid' => get_post_meta($post->ID, 'bgid', true),
+					'audioUrl' => get_post_meta($post->ID, 'audio_url', true),
+					'authorName' => get_post_meta($post->ID, 'author_name', true),
+					'authorTown' => get_post_meta($post->ID, 'author_town', true),
+				);
+				return (object) $speech;
+			}, $posts);
+
+			return rest_ensure_response($speeches);
+
+		}
 	}
 }
